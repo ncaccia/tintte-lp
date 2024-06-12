acedb32af43b0997d85168e844c154830d37d5ac
Nicolas Cacciavillani <ncacciavillani@gmail.com>
2024-06-12 18:15:04 +0200

    css improvements

---
+++ src/scripts/navBarScroll.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
